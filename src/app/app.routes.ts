a3307fea0506148c9c3a03c05ec8cf9a262438af
Jorge Chafloque Millones <37223352+jchafloquem@users.noreply.github.com>
2024-09-04 16:12:48 -0500

    Creacion de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,46 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const rutas: Routes = [
+  {
+    path: 'auth',
+    loadComponent: () => import('./auth/auth.component'),
+    children: [
+      {
+        path: 'bienvenido',
+        loadComponent: () => import('./auth/pages/welcome/welcome.component'),
+      },
+      {
+        path: 'login',
+        loadComponent: () => import('./auth/pages/login/login.component'),
+      },
+      {
+        path: 'registro',
+        loadComponent: () => import('./auth/pages/register/register.component'),
+      },
+    ],
+  },
+  {
+    path: 'geovisor',
+    loadComponent: () => import('./geovisor/geovisor.component'),
+    children: [
+      {
+        path: 'mapa',
+        loadComponent: () => import('./geovisor/pages/map/map.component'),
+      },
+      {
+        path:'',
+        redirectTo:'mapa',
+        pathMatch: 'full'
+      }
+    ],
+  },
+  {
+    path:'',
+    redirectTo:'geovisor',
+    pathMatch: 'full'
+  },
+  {
+    path:'**',
+    loadComponent: () => import('./shared/error/error.component')
+  }
+];
---
+++ src/app/geovisor/geovisor.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+
+@Component({
+  selector: 'app-geovisor',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './geovisor.component.html',
+  styles: ''
+})
+export default class GeovisorComponent {
+
+}
